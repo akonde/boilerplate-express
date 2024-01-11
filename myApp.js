ee481f0d936f008b699bfb114fdd8d7d6b6ebb74
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-01-11 21:22:52 +0000

    myapp updated

--- myApp.js
+++ myApp.js
@@ -20,7 +20,7 @@ let app = express();
 
 app.get('/', (req, res) => {
     // Send the "views/index.html" file as a response
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
   
 console.log("Hello World")
