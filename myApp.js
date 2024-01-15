2db41b83cabcdde767dc93398033d1fd36c86296
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-01-15 13:14:42 +0000

    json route added

--- myApp.js
+++ myApp.js
@@ -1,27 +1,23 @@
 let express = require('express');
 let app = express();
 
-// adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction).
-//  The first path argument is optional.If you don’t pass it, the middleware will be executed for all requests.
+// app.get('/', (req, res) => {
 
-// Mount the express.static() middleware to the path /public with app.use().
-//  The absolute path to the assets folder is __dirname + /public.
+//   app.use("/public", express.static(__dirname + "/public"));
+//     // Send the "views/index.html" file as a response
+//   res.sendFile(__dirname + "/views/index.html");
+  
+// });
 
-// Now your app should be able to serve a CSS stylesheet.
-//  Note that the / public / style.css file is referenced in the / views / index.html in the project boilerplate.
-// Your front - page should look a little better now!
+app.get('/json', (req, res) => {
 
-
-
-app.get('/', (req, res) => {
-
-  app.use("/public", express.static(__dirname + "/public"));
+  app.use("/json", express.static(__dirname + "/json"));
     // Send the "views/index.html" file as a response
-    res.sendFile(__dirname + "/views/index.html");
+  res.json({ "message": "Hello json" })
+  console.log("testing server")
 });
 
-
   
-console.log("Hello World")
+// console.log("Hello World")
 
  module.exports = app;
