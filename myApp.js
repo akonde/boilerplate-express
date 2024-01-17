c15face9cf47c0cb0bd323b4e26f216af7482c29
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-01-17 21:43:16 +0000

    json message update

--- myApp.js
+++ myApp.js
@@ -1,23 +1,26 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 
-//   app.use("/public", express.static(__dirname + "/public"));
-//     // Send the "views/index.html" file as a response
-//   res.sendFile(__dirname + "/views/index.html");
+  app.use("/public", express.static(__dirname + "/public"));
+    // Send the "views/index.html" file as a response
+  res.sendFile(__dirname + "/views/index.html");
   
-// });
+});
 
 app.get('/json', (req, res) => {
-
+  
   app.use("/json", express.static(__dirname + "/json"));
-    // Send the "views/index.html" file as a response
+  // Send the "views/index.html" file as a response
+  const messageStyle = process.env.MESSAGE_STYLE || 'uppercase';
+  const message = (messageStyle === 'uppercase') ? 'HELLO JSON' : 'Hello json'; 
   res.json({ "message": "Hello json" })
   console.log("testing server")
 });
 
   
-// console.log("Hello World")
-
+console.log("Hello World")
+         
  module.exports = app;
